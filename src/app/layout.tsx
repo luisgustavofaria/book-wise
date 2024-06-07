1185b60f4ea48227e68dbb277ba96c8770ff157d
Luis <luis.gustavo.fariac@gmail.com>
2024-06-07 00:07:42 -0300

    styled components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,18 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+'use client'
 
-const inter = Inter({ subsets: ['latin'] })
+import StyledComponentsRegistry from '@/lib/registry'
+import { GlobalStyle } from '@/styles/global'
+import theme from '@/styles/theme'
+import { ThemeProvider } from 'styled-components'
+//import type { Metadata } from 'next'
+// import { Inter } from 'next/font/google'
 
-export const metadata: Metadata = {
-  title: 'Book Wise',
-  description: 'Create next app',
-}
+// const inter = Inter({ subsets: ['latin'] })
+
+// export const metadata: Metadata = {
+//   title: 'Book Wise',
+//   description: 'Create next app',
+// }
 
 export default function RootLayout({
   children,
@@ -15,7 +21,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <title>Book Wise</title>
+        <meta name="description" content="Create next app" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <ThemeProvider theme={theme}>
+        <StyledComponentsRegistry>
+          <GlobalStyle />
+          <body>{children}</body>
+        </StyledComponentsRegistry>
+      </ThemeProvider>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+'use client'
+
+import styled from 'styled-components'
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.colors.gray[100]};
+`
+
 export default function Home() {
   return (
-    <>
+    <Container>
       <div>hello world</div>
-    </>
+    </Container>
   )
 }
